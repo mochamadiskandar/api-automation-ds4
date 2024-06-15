c94114e557ecdc689def377e19784b3bbbc55e46
Mochamad Iskandar - NTT <mochamadiskandar19@gmail.com>
2024-06-15 13:58:30 +0700

    prepare - install library

---
+++ test/api_test_template.js
@@ -0,0 +1,9 @@
+// describe('Test Suite Template ', () => {
+//     it('Test Case satu', () => {
+//         console.log('tc1')
+//     })
+
+//     it('Test Case dua', () => {
+//         console.log('tc2')
+//     })
+// })
